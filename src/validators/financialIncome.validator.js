9984c461225a472d2d84e23b78ca9b4611247bbf
leonardopadilha <leonardopadilha28@gmail.com>
2023-05-04 05:59:33 -0300

    Adicionando validação para campo vazio

--- src/validators/financialIncome.validator.js
+++ src/validators/financialIncome.validator.js
@@ -3,9 +3,9 @@ const {validatorMessage } = require('../utils/errorMessage')
 
 const postFinancialIncome = function() {
     return [
-        body('descricao', validatorMessage('Descrição')).exists().bail().isString(),
-        body('valor', validatorMessage('Valor')).exists().bail().isFloat(),
-        body('mes', validatorMessage('Mês')).exists().bail().isString()
+        body('descricao', validatorMessage('Descrição')).exists().notEmpty().bail().isString(),
+        body('valor', validatorMessage('Valor')).exists().notEmpty().bail().isFloat(),
+        body('mes', validatorMessage('Mês')).exists().notEmpty().bail().isString()
     ]
 }
 
