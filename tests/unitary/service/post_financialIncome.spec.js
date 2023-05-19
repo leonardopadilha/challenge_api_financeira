4c4e2bd36b529e7ae2213d9aa1a87abd729ad0a2
leonardopadilha <leonardopadilha28@gmail.com>
2023-05-19 03:17:46 -0300

    Iniciando testes unitários em receitas service

---
+++ tests/unitary/service/post_financialIncome.spec.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const objFinancial = require('../../../helpers/obj-course');
+const financialIncomeService = require('../../../src/service/financialIncome.service');
+
+const expect = chai.expect;
+
+describe('Suite', () => {
+    describe('Cadastro de receitas', () => {
+  
+        it ('Então a despesa financeira deve ser cadastrada com sucesso', async function () {
+           const financial = await financialIncomeService.postFinancialIncome(objFinancial)
+            expect(financial).to.contain(objFinancial)
+        })
+
+        it ('Então ao fazer o GET devem retornar os dados salvos no banco', async function () {
+            const getFinancial = await financialIncomeService.getFinancialIncome();
+            
+            const objSave = Object.values(getFinancial) 
+            expect(objSave[0].descricao).to.equals('Curso de programacao I')
+        })
+
+        it ('Então os dados devem apagados do banco', async function () {
+            await financialIncomeService.destroyFinancialIncome()
+            const returnFinancial = await financialIncomeService.getFinancialIncome();
+            
+            const msg = Object.values(returnFinancial)
+            expect(msg).to.contains("Sorry, we couldn't find any records for this search.")
+        })
+    })
+})
