2031ac907dc19daa4cf92084b8c5fddd6b5bf8fe
leonardopadilha <leonardopadilha28@gmail.com>
2023-05-21 07:13:35 -0300

    Adicionando array de curso em helper

---
+++ helpers/array-course.js
@@ -0,0 +1,6 @@
+const arrFinancial = [
+    { "descricao": "Curso de programacao II", "valor": 152.22, "mes": "Novembro" },
+    { "descricao": "Curso de programacao I", "valor": 152.22, "mes": "Novembro" }
+]
+
+module.exports = arrFinancial;
