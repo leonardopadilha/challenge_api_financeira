da862d9705d85feec8ec1491a75d41e75c663e82
leonardopadilha <leonardopadilha28@gmail.com>
2023-05-26 07:01:18 -0300

    Validando regra de duplicidade em receitas

---
+++ tests/integration/put_financialIncome_valid_rule.spec.js
@@ -0,0 +1,72 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const data = require('../helpers/financialExpenses.json')
+const getEnvString = require('../../helpers/get-env-string')
+const schema = require('../helpers/schema')
+const Joi = require('joi');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+const request = chai.request(getEnvString('BASE_URL'));
+
+const financial = [
+    { "descricao": "Curso Automacao", "valor": 152.22, "mes": "Novembro" },
+    { "descricao": "Curso Automacao API", "valor": 152.22, "mes": "Novembro" },
+    { "descricao": "Curso Automacao WEB", "valor": 152.22, "mes": "Novembro" }
+]
+
+let id_financial = "";
+
+financial.forEach(element => {
+    describe('Popular banco', () => {
+       
+        it('Salvar dados de receitas', (done) => {
+            request
+            .post(getEnvString('FINANCIAL_INCOME'))
+            .send(element)
+            .end((err, res) => {
+                expect(res).to.has.status(201)
+                Joi.assert(res.body, schema.postFinancialExpenses)
+                done();
+            })
+        })
+    })
+})
+
+describe('Evitar dados duplicados para o mesmo mês', () => {
+
+    it('Validar que as informações foram salvas', (done) => {
+        request
+            .get(getEnvString('FINANCIAL_INCOME'))
+            .end((err, res) => {
+                expect(res).to.has.status(200)
+                expect(res.body).to.an('array')
+
+                id_financial = res.body[2].id
+                done()
+            })
+    })
+
+    it('Sistema deve exibir mensagem de duplicidade', (done) => {
+        request
+            .put(getEnvString('FINANCIAL_INCOME') + id_financial)
+            .send(financial[0])
+            .end((err, res) => {
+                expect(res).to.has.status(409)
+                expect(res.body).to.not.have.property('id')
+                expect(res.body).to.contains("Sorry, finance income already exists")
+                done()
+            })
+    })
+})
+
+after((done) => {
+    request
+        .delete(getEnvString('FINANCIAL_INCOME'))
+        .end((err, res) => {
+            expect(res).to.has.status(200)
+            expect(res.body.message).to.equals("Successfully deleted information")
+            done()
+        })
+});
